bb0130fc33d3c96169248c1aa1ed122fb26b8f6b
Isaac Amezcua <iamezcua.dev@gmail.com>
2020-01-12 22:09:10 -0600

    Add Scala code related to the chapter "Generics"
    Takeaways
    - Generics allow us to use the same code on many potentially unrelated types.
      - It only works for classes and traits ( not for objects ).
      - You can define multiple type parameters.
    - The answer to the variance problem - "if B extends A, should List[B] extend List[A]?, has three different answers:
      - Covariance: a generic List of type A can accept either, elements of type A and their subclasses.
      - Invariance: a generic List of type A can accept elements of type A only.
      - Contravariance: a generic List of type A can accept either, elements of type A and their superclasses.
    - We use bounded types to restrict the data types a generic class/trait can accept:
      - Upper-bound operator "<:", restricts A to accept A or its subclasses.
      - Lower-bound operator "<:", restricts A to accept A or its superclasses.
    - We use the lower-bound operator to solve the annoying variance problem: "covariant type A occurs in contravariant position in type A of value `yourVariable`", which occurs in covariant generic classes/functions by using the following technique:
      - def myFunction[ B >: A ]( yourVariable: B ): YourClass[B]

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -2,7 +2,8 @@ package exercises
 
 import com.typesafe.scalalogging.LazyLogging
 
-abstract class MyList {
+// Covariant class
+abstract class MyList[ +A ] {
 	/*
 		head = first element of  the list
 		tail = remaider of the list
@@ -10,34 +11,53 @@ abstract class MyList {
 		add(int) => new list with thisx element added
 		toString => a string representation of the list
 	 */
-	def head: Int
-	def tail: MyList
+	def head: A
+	def tail: MyList[ A ]
 	def isEmpty: Boolean
-	def add( number: Int ): MyList
+	def add[ B >: A ]( number: B ): MyList[ B ]
 	def listContents: String
 	override def toString: String = s"[ $listContents ]"
 }
 
-object EmptyList extends MyList {
-	def head: Int = throw new NoSuchElementException
-	def tail: MyList = throw new NoSuchElementException
+object EmptyList extends MyList[ Nothing ] {
+	def head: Nothing = throw new NoSuchElementException
+	def tail: Nothing = throw new NoSuchElementException
 	def isEmpty: Boolean = true
-	def add( element: Int ): MyList = new Node( element, EmptyList )
+	def add[ B >: Nothing ]( element: B ): MyList[ B ] = new Node( element, EmptyList )
 	def listContents: String = ""
 }
 
-class Node( element: Int, listTail: MyList ) extends MyList {
-	def head: Int = element
-	def tail: MyList = this.listTail
+class Node[ +A ]( element: A, listTail: MyList[ A ] ) extends MyList[ A ] {
+	def head: A = element
+	def tail: MyList[ A ] = this.listTail
 	def isEmpty: Boolean = false
-	def add( element: Int ): MyList = new Node( element, this )
+	// B supertype of Nothing
+	def add[ B >: A ]( element: B ): MyList[ B ] = new Node( element, this )
 	def listContents: String = {
-		if ( listTail.isEmpty ) s"$element"
-		else s"$element ${listTail.listContents}"
+		if ( listTail.isEmpty ) s"$element (${element.getClass.getSimpleName})"
+		else s"$element (${element.getClass.getSimpleName}), ${listTail.listContents}"
 	}
 }
 
 object MyApp extends App with LazyLogging {
 	val list = new Node( 1, new Node( 2, new Node( 3, new Node( 5, EmptyList ) ) ) )
 	logger.info( list.toString )
+	
+	// Case 1: List of zero elements 	-> [  ]
+	println( EmptyList )
+	assert( EmptyList.toString equals "[  ]" )
+	
+	// Case 2: List of 1 element 			-> [ 1 (Integer) ]
+	println( EmptyList.add( 1 ) )
+	assert( EmptyList.add( 1 ).toString equals "[ 1 (Integer) ]" )
+	
+	// Case 3: List of 2 elements			-> [ 1 (Integer), 2 (Integer) ]
+	println( EmptyList.add( 2 ).add( 1 ) )
+	assert( EmptyList.add( 2 ).add( 1 ).toString equals "[ 1 (Integer), 2 (Integer) ]" )
+	
+	// Case 4: List of 4 elements			-> [ 1 (Integer), Isaac (String), 3 (Integer), Amezcua (String) ]
+	println( EmptyList.add( "Amezcua" ).add( 3 ).add( "Isaac" ).add( 1 ) )
+	assert( EmptyList.add( "Amezcua" ).add( 3 ).add( "Isaac" ).add( 1 ).toString equals "[ 1 (Integer), Isaac (String), 3 (Integer), Amezcua (String) ]" )
+	
 }
+
