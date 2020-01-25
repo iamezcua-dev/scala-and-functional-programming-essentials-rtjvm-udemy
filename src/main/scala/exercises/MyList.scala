5e70a8a86ce1cd932177d2fe34f7f85eab27c8f4
Isaac Amezcua <iamezcua.dev@gmail.com>
2020-01-24 23:24:58 -0600

    Add code related to the attempt to solve the exercises related to the chapter "Object-Oriented Exercises: Expanding Our Collection"

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -17,6 +17,9 @@ abstract class MyList[ +A ] {
 	def add[ B >: A ]( number: B ): MyList[ B ]
 	def listContents: String
 	override def toString: String = s"[ $listContents ]"
+	def map[ B >: A ]( transformer: B => B ): MyList[ B ]
+	def filter[ B >: A ]( predicate: B => Boolean ): MyList[ B ]
+	def flatMap[ B >: A ]( transformer: B => MyList[ B ] ): MyList[ B ]
 }
 
 object EmptyList extends MyList[ Nothing ] {
@@ -25,6 +28,9 @@ object EmptyList extends MyList[ Nothing ] {
 	def isEmpty: Boolean = true
 	def add[ B >: Nothing ]( element: B ): MyList[ B ] = new Node( element, EmptyList )
 	def listContents: String = ""
+	override def map[ B >: Nothing ]( transformer: B => B ): MyList[ B ] = this
+	override def filter[ B >: Nothing ]( predicate: B => Boolean ): MyList[ B ] = this
+	override def flatMap[ B >: Nothing ]( transformer: B => MyList[ B ] ): MyList[ B ] = this
 }
 
 class Node[ +A ]( element: A, listTail: MyList[ A ] ) extends MyList[ A ] {
@@ -37,6 +43,36 @@ class Node[ +A ]( element: A, listTail: MyList[ A ] ) extends MyList[ A ] {
 		if ( listTail.isEmpty ) s"$element (${element.getClass.getSimpleName})"
 		else s"$element (${element.getClass.getSimpleName}), ${listTail.listContents}"
 	}
+	
+	override def map[ B >: A ]( transformer: B => B ): MyList[ B ] = {
+		if ( listTail.isEmpty ) new Node( transformer( element ), EmptyList )
+		else {
+			new Node( transformer( element ), listTail.map( transformer ) )
+		}
+	}
+	
+	override def filter[ B >: A ]( predicate: B => Boolean ): MyList[ B ] = {
+		if ( listTail.isEmpty ) {
+			if ( predicate( element ) ) {
+				new Node( element, EmptyList )
+			} else {
+				EmptyList
+			}
+		} else {
+			if ( predicate( element ) ) {
+				new Node( element, tail.filter( predicate ) )
+			} else {
+				tail.filter( predicate )
+			}
+		}
+	}
+	
+	override def flatMap[ B >: A ]( transformer: B => MyList[ B ] ): MyList[ B ] = {
+		val mappedElement = transformer( element )
+		if ( listTail.isEmpty ) new Node( mappedElement.head, new Node( mappedElement.tail.head, EmptyList ) )
+		else new Node( mappedElement.head, new Node( mappedElement.tail.head, listTail.flatMap( transformer ) ) )
+	}
+	
 }
 
 object MyApp extends App with LazyLogging {
--- src/main/scala/lectures/part2/oop/AnonymousClasses.scala
+++ src/main/scala/lectures/part2/oop/AnonymousClasses.scala
@@ -1,6 +1,9 @@
 package lectures.part2.oop
 
-object AnonymousClasses extends App {
+import com.typesafe.scalalogging.LazyLogging
+import exercises.{ EmptyList, MyList, Node }
+
+object AnonymousClasses extends App with LazyLogging {
 	abstract class Animal {
 		def eat: Unit
 	}
@@ -29,4 +32,76 @@ object AnonymousClasses extends App {
 	}
 	
 	jim.sayHi
+	
+	/*
+		1. Generic trait MyPredicate[-T] with a little method test(T) => boolean
+		2. Generic trait MyTransformer[-A, B] with a method transform(A) => B
+		3. Implement the following methods on MyList:
+			- map( transformer ) => MyList
+			- filter( predicate) => MyList
+			- flatMap( transformer from A to MyList[B])
+			
+			class EvenPredicate extends MyPredicate[Int]
+			class StringToIntTransformer extends MyTransformer[String, Int]
+			
+			[1,2,3].map(n * 2) = [2,4,6]
+			[1,2,3,4].filter(n % 2) = [2,4]
+			[1,2,3].flatMap( n => [n,n+1]) => [1,2,2,3,3,4]
+	 */
+	
+	// Exercises solutions
+	trait MyPredicate[ -T ] {
+		def test( element: T ): Boolean
+	}
+	
+	trait MyTransformer[ -A, B ] {
+		def transform( element: A ): B
+	}
+	
+	val list = EmptyList.add( 15 ).add( 12 ).add( 6 ).add( 5 ).add( 4 ).add( 3 ).add( 2 ).add( 1 )
+	logger.info( s"Original list: ${list.toString}" )
+	
+	/*
+		map function implementation
+	 */
+	logger.info( "Map function implementation" )
+	class MultiplierTransformer extends MyTransformer[ Int, Int ] {
+		override def transform( thing: Int ): Int = thing * 2
+	}
+	val multiplierTransformer = new MultiplierTransformer
+	val multipliedList = list.map( multiplierTransformer.transform )
+	println( multipliedList )
+	assert( multipliedList.toString equals "[ 2 (Integer), 4 (Integer), 6 (Integer), 8 (Integer), 10 (Integer), " +
+			"12 (Integer), 24 (Integer), 30 (Integer) ]" )
+	assert( EmptyList.map( multiplierTransformer.transform ).toString.equals( "[  ]" ) )
+	
+	/*
+		filter function implementation
+	 */
+	logger.info( "Filter function implementation" )
+	class EvenPredicate extends MyPredicate[ Int ] {
+		override def test( element: Int ): Boolean = element % 2 == 0
+	}
+	val evenPredicate = new EvenPredicate
+	val filteredList = list.filter( evenPredicate.test )
+	println( filteredList )
+	assert( filteredList.toString.equals( "[ 2 (Integer), 4 (Integer), 6 (Integer), 12 (Integer) ]" ) )
+	assert( EmptyList.filter( evenPredicate.test ).toString.equals( "[  ]" ) )
+	
+	/*
+		flatMap function implementation
+	 */
+	//	[1,2,3].flatMap( n => [n,n+1]) => [1,2,2,3,3,4]
+	logger.info( "FlatMap function implementation" )
+	class ElementAndElementPlusOneTransformer extends MyTransformer[ Int, MyList[ Int ] ] {
+		override def transform( element: Int ): MyList[ Int ] = new Node( element, new Node( element + 1, EmptyList ) )
+	}
+	val elementAndElementPlusOneTransformer = new ElementAndElementPlusOneTransformer
+	val flatMappedList = list.flatMap( elementAndElementPlusOneTransformer.transform )
+	println( flatMappedList )
+	assert( flatMappedList.toString equals "[ 1 (Integer), 2 (Integer), 2 (Integer), 3 (Integer), 3 (Integer)," +
+			" 4 (Integer), 4 (Integer), 5 (Integer), 5 (Integer), 6 (Integer), 6 (Integer), 7 (Integer), 12 (Integer)," +
+			" 13 (Integer), 15 (Integer), 16 (Integer) ]" )
+	assert( EmptyList.flatMap( elementAndElementPlusOneTransformer.transform ).toString.equals( "[  ]" ) )
+	
 }
