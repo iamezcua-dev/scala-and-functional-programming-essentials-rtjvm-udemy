1dba44d031e3c08cb1c1fe8e21fe7c3ae2fbcf17
Isaac Amezcua <iamezcua.dev@gmail.com>
2020-06-07 00:41:44 -0500

    Move my implementation of the higher-order function implementation exercises to the correct file
    - This this code belongs to the chapter 'Object-Oriented Exercises: Expanding Our Collection'

--- src/main/scala/lectures/part2/oop/AnonymousClasses.scala
+++ src/main/scala/lectures/part2/oop/AnonymousClasses.scala
@@ -49,48 +49,6 @@ object AnonymousClasses extends App with LazyLogging {
 			[1,2,3].flatMap( n => [n,n+1]) => [1,2,2,3,3,4]
 	 */
 
-	// Solutions to the exercises
-	val list = EmptyList.add( 15 ).add( 12 ).add( 6 ).add( 5 ).add( 4 ).add( 3 ).add( 2 ).add( 1 )
-	logger.info( s"Original list: ${list.toString}" )
-
-	/*
-		map function implementation
-	 */
-	logger.info( "* Map function implementation" )
-	val multiplierTransformer = new (Int => Int) {
-		override def apply( thing: Int ): Int = thing * 2
-	}
-	val multipliedList = list.map( multiplierTransformer )
-	assert( multipliedList.toString equals "[ 2 (Integer), 4 (Integer), 6 (Integer), 8 (Integer), 10 (Integer), " +
-  "12 (Integer), 24 (Integer), 30 (Integer) ]" )
-  assert( EmptyList.map( multiplierTransformer ).toString.equals( "[  ]" ) )
-  logger.info( multipliedList.toString )
-
-	/*
-		filter function implementation
-	 */
-	logger.info( "* Filter function implementation" )
-	val evenPredicate = new (Int => Boolean) {
-		override def apply( element: Int ): Boolean = element % 2 == 0
-	}
-	val filteredList = list.filter( evenPredicate )
-	assert( filteredList.toString.equals( "[ 2 (Integer), 4 (Integer), 6 (Integer), 12 (Integer) ]" ) )
-	assert( EmptyList.filter( evenPredicate ).toString.equals( "[  ]" ) )
-	logger.info( filteredList.toString )
-
-	/*
-		flatMap function implementation
-	 */
-	logger.info( "* FlatMap function implementation" )
-
-	val elementAndElementPlusOneTransformer = new (Int => MyList[ Int ]) {
-		override def apply( element: Int ): MyList[ Int ] = Node( element, Node( element + 1, EmptyList ) )
-	}
-	val flatMappedList = list.flatMap( elementAndElementPlusOneTransformer )
-	assert( flatMappedList.toString equals "[ 1 (Integer), 2 (Integer), 2 (Integer), 3 (Integer), 3 (Integer)," +
-  " 4 (Integer), 4 (Integer), 5 (Integer), 5 (Integer), 6 (Integer), 6 (Integer), 7 (Integer), 12 (Integer)," +
-  " 13 (Integer), 15 (Integer), 16 (Integer) ]" )
-  assert( EmptyList.flatMap( elementAndElementPlusOneTransformer ).toString.equals( "[  ]" ) )
-  logger.info( flatMappedList.toString )
+	// Solutions to the exercises are in the MyList class
 
 }
