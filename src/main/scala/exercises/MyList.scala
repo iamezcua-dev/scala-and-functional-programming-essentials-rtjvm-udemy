a7b28edee5c40cd095972f1bcdc4f5579301586d
Isaac Amezcua <iamezcua.dev@gmail.com>
2020-07-30 19:33:26 -0500

    Reformat code using correct IntelliJ IDEA's code style

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -45,23 +45,22 @@ case class Node[ +A ]( element: A, listTail: MyList[ A ] ) extends MyList[ A ] {
 		if ( listTail.isEmpty ) s"$element (${element.getClass.getSimpleName})"
 		else s"$element (${element.getClass.getSimpleName}), ${listTail.listContents}"
 	}
-
+	
 	override def map[ B ]( transformer: A => B ): MyList[ B ] =
 		Node( transformer( element ), listTail.map( transformer ) )
-
+	
 	/*
 		[1, 2, 3, 4].filter( n % 2 == 0 )
 		= predicate( 1 % 2 == 0 ) is false. Then, continuing with: [2, 3, 4].filter( n % 2 == 0 )
 		= predicate( 2 % 2 == 0 ) is true. Then, adding it to the list and continuing with: [2], [3, 4].filter( n % 2 == 0 )
 		= predicate( 3 % 2 == 0 ) is false. Then, continuing with: [2], [4].filter( n % 2 == 0 )
 		= predicate( 4 % 2 == 0 ) is true. Then, adding it to the list and continuing with: [2], [3].filter( n % 2 == 0 )
-
 	 */
 	override def filter( predicate: A => Boolean ): MyList[ A ] = {
 		if ( predicate( element ) ) Node( element, tail.filter( predicate ) )
 		else tail.filter( predicate )
 	}
-
+	
 	/*
 		[ 1, 2, 3].flatMap(  n => [ n, n+1 ] )
 		= [1, 2] ++ [2, 3].flatMap( transformer )
@@ -71,7 +70,7 @@ case class Node[ +A ]( element: A, listTail: MyList[ A ] ) extends MyList[ A ] {
 	 */
 	override def flatMap[ B ]( transformer: A => MyList[ B ] ): MyList[ B ] =
 		transformer( element ) ++ listTail.flatMap( transformer )
-
+	
 	/*
 			list ++ new Node( 27, new Node( 30, EmptyList ) ) )
 
@@ -97,84 +96,84 @@ case class Node[ +A ]( element: A, listTail: MyList[ A ] ) extends MyList[ A ] {
 case object MyApp extends App with LazyLogging {
 	val myList = Node( 1, Node( 2, Node( 3, Node( 5, EmptyList ) ) ) )
 	logger.info( myList.toString )
-
+	
 	// Case 1: List of zero elements 	-> [  ]
 	println( EmptyList )
 	assert( EmptyList.toString equals "[  ]" )
-
+	
 	// Case 2: List of 1 element 			-> [ 1 (Integer) ]
 	println( EmptyList.add( 1 ) )
 	assert( EmptyList.add( 1 ).toString equals "[ 1 (Integer) ]" )
-
+	
 	// Case 3: List of 2 elements			-> [ 1 (Integer), 2 (Integer) ]
 	println( EmptyList.add( 2 ).add( 1 ) )
 	assert( EmptyList.add( 2 ).add( 1 ).toString equals "[ 1 (Integer), 2 (Integer) ]" )
-
+	
 	// Case 4: List of 4 elements			-> [ 1 (Integer), Isaac (String), 3 (Integer), Amezcua (String) ]
 	println( EmptyList.add( "Amezcua" ).add( 3 ).add( "Isaac" ).add( 1 ) )
 	assert( EmptyList.add( "Amezcua" ).add( 3 ).add( "Isaac" ).add( 1 ).toString equals "[ 1 (Integer), Isaac (String), 3 (Integer), Amezcua (String) ]" )
-
+	
 	/*
 		My own implementation
 	 */
 	println( "\n\n*** My own implementation ***" )
-
+	
 	val list = EmptyList.add( 15 ).add( 12 ).add( 6 ).add( 5 ).add( 4 ).add( 3 ).add( 2 ).add( 1 )
 	logger.info( s"Original list: ${list.toString}" )
-
+	
 	// map function implementation
 	logger.info( "* Map function implementation" )
-	val multiplierTransformer = new (Int => Int) {
+	val multiplierTransformer = new ( Int => Int ) {
 		override def apply( thing: Int ): Int = thing * 2
 	}
 	val multipliedList = list.map( multiplierTransformer )
 	assert( multipliedList.toString equals "[ 2 (Integer), 4 (Integer), 6 (Integer), 8 (Integer), 10 (Integer), " +
-		"12 (Integer), 24 (Integer), 30 (Integer) ]" )
+			"12 (Integer), 24 (Integer), 30 (Integer) ]" )
 	assert( EmptyList.map( multiplierTransformer ).toString.equals( "[  ]" ) )
 	logger.info( multipliedList.toString )
-
+	
 	// filter function implementation
 	logger.info( "* Filter function implementation" )
-	val evenPredicate = new (Int => Boolean) {
+	val evenPredicate = new ( Int => Boolean ) {
 		override def apply( element: Int ): Boolean = element % 2 == 0
 	}
 	val filteredList = list.filter( evenPredicate )
 	assert( filteredList.toString.equals( "[ 2 (Integer), 4 (Integer), 6 (Integer), 12 (Integer) ]" ) )
 	assert( EmptyList.filter( evenPredicate ).toString.equals( "[  ]" ) )
 	logger.info( filteredList.toString )
-
+	
 	// flatMap function implementation
 	logger.info( "* FlatMap function implementation" )
-
-	val elementAndElementPlusOneTransformer = new (Int => MyList[ Int ]) {
+	
+	val elementAndElementPlusOneTransformer = new ( Int => MyList[ Int ] ) {
 		override def apply( element: Int ): MyList[ Int ] = Node( element, Node( element + 1, EmptyList ) )
 	}
 	val flatMappedList = list.flatMap( elementAndElementPlusOneTransformer )
 	assert( flatMappedList.toString equals "[ 1 (Integer), 2 (Integer), 2 (Integer), 3 (Integer), 3 (Integer)," +
-		" 4 (Integer), 4 (Integer), 5 (Integer), 5 (Integer), 6 (Integer), 6 (Integer), 7 (Integer), 12 (Integer)," +
-		" 13 (Integer), 15 (Integer), 16 (Integer) ]" )
+			" 4 (Integer), 4 (Integer), 5 (Integer), 5 (Integer), 6 (Integer), 6 (Integer), 7 (Integer), 12 (Integer)," +
+			" 13 (Integer), 15 (Integer), 16 (Integer) ]" )
 	assert( EmptyList.flatMap( elementAndElementPlusOneTransformer ).toString.equals( "[  ]" ) )
 	logger.info( flatMappedList.toString )
-
-
+	
+	
 	/*
 		Author's code
 	 */
 	println( "\n\n*** Author's code ***" )
-
-	val listOfIntegers: MyList[Int] = Node( 1, Node(2, Node( 3, EmptyList ) ) )
-	val anotherListOfIntegers: MyList[Int] = Node( 1, Node( 4, Node( 3, EmptyList ) ) )
-	val listOfStrings: MyList[String] = Node( "Hello", Node( "Scala", EmptyList ) )
-
-	println( listOfIntegers)
+	
+	val listOfIntegers: MyList[ Int ] = Node( 1, Node( 2, Node( 3, EmptyList ) ) )
+	val anotherListOfIntegers: MyList[ Int ] = Node( 1, Node( 4, Node( 3, EmptyList ) ) )
+	val listOfStrings: MyList[ String ] = Node( "Hello", Node( "Scala", EmptyList ) )
+	
+	println( listOfIntegers )
 	println( listOfStrings )
-
+	
 	println( listOfIntegers.map( _ * 2 ).toString )
-
+	
 	println( listOfIntegers.filter( _ % 2 == 0 ).toString )
-
+	
 	println( listOfIntegers ++ anotherListOfIntegers )
-	println( listOfIntegers.flatMap( (elem: Int) => Node( elem, Node( elem + 1, EmptyList ) ) ).toString )
-
+	println( listOfIntegers.flatMap( ( elem: Int ) => Node( elem, Node( elem + 1, EmptyList ) ) ).toString )
+	
 }
 
