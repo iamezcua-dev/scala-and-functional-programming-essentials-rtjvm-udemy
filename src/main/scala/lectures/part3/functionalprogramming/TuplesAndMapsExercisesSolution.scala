dd01cf7b7faa29c5fdab834afec44f6e2de878c5
Isaac Amezcua <iamezcua.dev@gmail.com>
2021-11-09 10:18:31 -0600

    Add pending implementation for the social network
    The  function implements breadth-first search algorithm to look for next neighbors.

--- src/main/scala/lectures/part3/functionalprogramming/TuplesAndMapsExercisesSolution.scala
+++ src/main/scala/lectures/part3/functionalprogramming/TuplesAndMapsExercisesSolution.scala
@@ -1,5 +1,7 @@
 package lectures.part3.functionalprogramming
 
+import scala.annotation.tailrec
+
 object TuplesAndMapsExercisesSolution extends App {
   /*
     1. What would happen if I had two original entries "Jim" -> 555 and "JIM" -> 900?
@@ -58,6 +60,76 @@ object TuplesAndMapsExercisesSolution extends App {
   
   println(testNet)
   
+  
+  // Mary, Peter, John, Jim, Bob, Stu
+  val people2 =
+    add(
+      add(
+        add(
+          add(
+            add(
+              add(
+                add(
+                  add(empty, "Mary"),
+                  "Peter"),
+                "John"),
+              "Jim"),
+            "Bob"),
+          "Stu"),
+        "Gerard"),
+      "Brandon")
+  
+  val maryFriends =
+    friend(
+      friend(
+        friend(
+          friend(people2,
+            "Mary", "Bob"),
+          "Mary", "Jim"),
+        "Mary", "John"),
+      "Mary", "Peter")
+  
+  val peterFriends =
+    friend(
+      friend(
+        friend(maryFriends,
+          "Peter", "John"),
+        "Peter", "Mary"),
+      "Peter", "Stu")
+  
+  val johnFriends =
+    friend(
+      friend(
+        friend(peterFriends,
+          "John", "Mary"),
+        "John", "Jim"),
+      "John", "Peter")
+  
+  val jimFriends =
+    friend(
+      friend(johnFriends,
+        "Jim", "John"),
+      "Jim", "Mary")
+  
+  val bobFriends =
+    friend(
+      friend(
+        friend(jimFriends,
+          "Bob", "Mary"),
+        "Bob", "Stu"),
+      "Bob", "Gerard")
+  
+  val gerardFriends =
+    friend(bobFriends,
+      "Gerard", "Bob")
+  
+  val bigSocialNetwork =
+    friend(
+      friend(gerardFriends,
+        "Stu", "Bob"),
+      "Stu", "Peter")
+  
+  
   def nFriends(network: Map[String, Set[String]], person: String): Int =
     if(!network.contains(person)) 0
     else network(person).size
@@ -74,8 +146,37 @@ object TuplesAndMapsExercisesSolution extends App {
   
   println(nPeopleWithNoFriends(testNet))
   
+  /**
+   * Checks whether <code>a</code> (or any of their friends) has a social connection with <code>b</code>
+   *
+   * @param network The social group to look into.
+   * @param a       The person in question.
+   * @param b       The person that may have social connection with <code>a</code>.
+   * @return Whether or not <code>a</code> has a direct or indirect social connection with <code>b</code>.
+   */
   def socialConnection(network: Map[String, Set[String]], a: String, b: String): Boolean = {
-    ???
+    @tailrec
+    def bfs(target: String, consideredPeople: Set[String], discoveredPeople: Set[String]): Boolean = {
+      if(discoveredPeople.isEmpty) false
+      else {
+        val person = discoveredPeople.head
+        if(person == target) true
+        else if(consideredPeople.contains(person)) bfs(target, consideredPeople, discoveredPeople.tail)
+        else bfs(target, consideredPeople + person, discoveredPeople.tail ++ network(person))
+      }
+    }
+    
+    bfs(b, Set(), network(a) + a)
   }
+  
+  println(s"*** Test network contents:\n$testNet")
+  println(socialConnection(testNet, "Mary", "Jim"))
+  println(socialConnection(network, "Mary", "Bob"))
+  
+  people2.foreach(x =>
+    people2.foreach(y =>
+      println(s"Has ${x._1} a social connection to ${y._1}? ${socialConnection(bigSocialNetwork, s"${x._1}", s"${y._1}")}")
+    )
+  )
   
 }
