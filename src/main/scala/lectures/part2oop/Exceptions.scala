98eea1905a6fe91a653175a6e9f3988b9140a1ee
Isaac Amezcua <iamezcua.dev@gmail.com>
2023-07-25 23:12:28 -0400

    Add solutions to the exercises of the Exceptions section

--- src/main/scala/lectures/part2oop/Exceptions.scala
+++ src/main/scala/lectures/part2oop/Exceptions.scala
@@ -13,7 +13,7 @@ object Exceptions extends App {
   // Exception and Error are the major Throwable subtypes.
 
   // 2. How to catch exceptions?
-  def getInt(withExceptions: Boolean): Int =
+  private def getInt(withExceptions: Boolean): Int =
     if withExceptions then throw new RuntimeException("No Int for you!")
     else 42
 
@@ -49,4 +49,83 @@ object Exceptions extends App {
         - UnderflowException if subtract(x, y) exceeds Int.MIN_VALUE
         - MathCalculationException for division by 0
   */
+
+  // 1. Crashing my program with an OOM exception
+  try {
+    val myArray = Array.ofDim[Int](Int.MaxValue)
+  } catch {
+    case e: OutOfMemoryError => println("OOM exception handled!")
+    case _ => println(s"Unexpected exception was handled instead.")
+  }
+
+  // 2. Crashing my program with SOError
+  private def infinite: Int = 1 + infinite
+
+  try {
+    val noLimit = infinite
+  } catch {
+    case e: StackOverflowError => println("SOE handled!")
+    case _ => println(s"Unexpected exception was handled instead.")
+  }
+
+  // 3. Pocket Calculator
+  class OverflowException extends Exception
+
+  class UnderflowException extends Exception
+
+  class MathCalculationException extends ArithmeticException
+
+  class PocketCalculator {
+    def add(x: Int, y: Int): Int = {
+      val result = x + y
+      if x >= 0 && y >= 0 && result < 0 then throw new OverflowException
+      result
+    }
+
+    def subtract(x: Int, y: Int): Int = {
+      val result = x - y
+      if x < 0 && y > x && result > 0 then throw new UnderflowException
+      result
+    }
+
+    def multiply(x: Int, y: Int): Int = x * y
+
+    def divide(x: Int, y: Int): Int = {
+      if y == 0 then throw new MathCalculationException
+      x / y
+    }
+  }
+
+  object PocketCalculator {
+    def apply(): PocketCalculator = new PocketCalculator()
+  }
+
+  private val calc = PocketCalculator()
+  private val a_sum = Int.MaxValue
+  private val b_sum = 8
+  private val a_sub = Int.MinValue
+  private val b_sub = 10
+  private val a_mult = 8
+  private val b_mult = 0
+  private val a_div = 9
+  private val b_div = 0
+
+  private lazy val sum = calc.add(a_sum, b_sum)
+  private lazy val subtraction = calc.subtract(a_sub, b_sub)
+  private lazy val multiplication = calc.multiply(a_mult, b_mult)
+  private lazy val division = calc.divide(a_div, b_div)
+
+
+  try println(sum)
+  catch case overflow: OverflowException => println(s"Summing up $a_sum to $b_sum would overflow Int capacity.")
+
+  try println(subtraction)
+  catch case underflow: UnderflowException => println(s"Subtracting $a_sub to $b_sub would underflow Int capacity.")
+
+  try println(multiplication)
+  catch case _ => println("Unexpected error occurred")
+
+  try println(division)
+  catch case zeroDivision: MathCalculationException => println(s"Dividing $a_div by $b_div would yield infinity.")
+
 }
