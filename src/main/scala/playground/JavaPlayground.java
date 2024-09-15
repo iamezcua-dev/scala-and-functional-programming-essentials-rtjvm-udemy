d687d1f40b0f6917a4df4266edb366e1ff2c01b2
Isaac Amezcua <iamezcua.dev@gmail.com>
2024-09-15 17:20:31 -0600

    Refactor Playground structure and add logging.
    - Change to package `com.rockthejvm` for both Java and Scala files.
    - Add logging capabilities to ScalaPlayground.

--- src/main/scala/playground/JavaPlayground.java
+++ src/main/scala/playground/JavaPlayground.java
@@ -1,13 +1,13 @@
-package playground;
+package com.rockthejvm.playground;
 
 // class which makes the analogies between Scala objects and Java statics
 public class JavaPlayground {
-    public static void main(String args[]) {
-        System.out.println(Person.N_EYES);
-    }
+  public static void main(String args[]) {
+    System.out.println(Person.N_EYES);
+  }
 }
 
 class Person {
-    public static final int N_EYES = 2;
+  public static final int N_EYES = 2;
 }
 
