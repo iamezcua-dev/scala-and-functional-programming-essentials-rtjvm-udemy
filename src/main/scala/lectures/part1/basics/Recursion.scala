eeaf5062c5a2a4fe695d0589a3224e3c34a0c415
Isaac Amezcua <iamezcua.dev@gmail.com>
2024-09-18 20:40:18 -0600

    Add solutions to the proposed exercises in the Recursion chapter

--- src/main/scala/lectures/part1/basics/Recursion.scala
+++ src/main/scala/lectures/part1/basics/Recursion.scala
@@ -31,9 +31,80 @@ object Recursion extends App with LazyLogging {
     factorialHelper(n, 1)
   }
   
-  logger.info(anotherFactorial(50000).toString)
+  //  logger.info(anotherFactorial(50000).toString)
   
   /**
    * Rule of thumb: When you need loops, use TAIL recursion whenever is possible.
    */
+  
+  /**
+   * Exercises:
+   *
+   *  - 1. Concatenate a string n times
+   *  - 2. IsPrime function tail recursive
+   *  - 3. Fibonacci function, tail recursive.
+   */
+  // 1.
+  private def repeatString(text: String, times: Int): String =
+    @tailrec
+    def repeatStringHelper(remaining: Int, accum: String): String =
+      if (remaining == 0) accum
+      else repeatStringHelper(remaining - 1, accum + text)
+    
+    repeatStringHelper(times, "")
+  
+  assert(repeatString("EddieJobs", 3).equals("EddieJobsEddieJobsEddieJobs"))
+  assert(repeatString("Rana", 10).equals("RanaRanaRanaRanaRanaRanaRanaRanaRanaRana"))
+  assert(repeatString("Quechoni", 8).equals("QuechoniQuechoniQuechoniQuechoniQuechoniQuechoniQuechoniQuechoni"))
+  
+  // 2.
+  private def isPrime(number: Int): Boolean = {
+    @tailrec
+    def isPrimeUntil(current: Int): Boolean = {
+      if (current <= 1) true
+      else number % current != 0 && isPrimeUntil(current - 1)
+    }
+    
+    number >= 2 && isPrimeUntil(number / 2)
+  }
+  
+  // Assertions for known prime numbers
+  assert(isPrime(2), "2 should be prime")
+  assert(isPrime(3), "3 should be prime")
+  assert(isPrime(5), "5 should be prime")
+  assert(isPrime(7), "7 should be prime")
+  assert(isPrime(11), "11 should be prime")
+  
+  // Assertions for known non-prime numbers
+  assert(!isPrime(0), "0 should not be prime")
+  assert(!isPrime(1), "1 should not be prime")
+  assert(!isPrime(4), "4 should not be prime")
+  assert(!isPrime(6), "6 should not be prime")
+  assert(!isPrime(9), "9 should not be prime")
+  
+  // 3.
+  private def fibonacci(number: Int): Int = {
+    @tailrec
+    def fibonacciHelper(remaining: Int, a: Int, b: Int): Int = {
+      if (remaining <= 1) b
+      else fibonacciHelper(remaining - 1, b, a + b)
+    }
+    
+    if (number < 0) 0
+    else if (number <= 1) number
+    else fibonacciHelper(number, 0, 1)
+  }
+  
+  assert(fibonacci(-1) == 0, "-1 should return 0")
+  assert(fibonacci(0) == 0)
+  assert(fibonacci(1) == 1)
+  assert(fibonacci(2) == 1)
+  assert(fibonacci(3) == 2)
+  assert(fibonacci(4) == 3)
+  assert(fibonacci(5) == 5)
+  assert(fibonacci(6) == 8)
+  assert(fibonacci(7) == 13)
+  assert(fibonacci(8) == 21)
+  assert(fibonacci(9) == 34)
+  
 }
